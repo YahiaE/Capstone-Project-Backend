0962ed3827d45cd2e75b86c5b99f2d3da04418cc
YahiaE <yahiae2002@gmail.com>
2022-01-27 13:00:05 -0500

    add img attribute to recipe

--- server/db/recipe.js
+++ server/db/recipe.js
@@ -2,26 +2,30 @@ const Sequelize = require('sequelize')
 const db = require('./connection')
 
 const Recipe = db.define('recipe', {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.TEXT,
-    allowNull: true
-  },
-  level_of_diff: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
-  time: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  steps: {
-    type: Sequelize.TEXT,
-    allowNull: false
-  }
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: true
+    },
+    level_of_diff: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    time: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    steps: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    img: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
 })
 
 module.exports = Recipe
